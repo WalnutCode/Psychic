d746d04485f6be61195abf4454198fd00c87c5e9
Jennifer <jennifer@Jennifers-Air.fios-router.home>
2018-06-07 16:46:30 -0400

    adding on

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+
 console.log("is this working?")
 var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s","t","u", "v", "w", "x", "y", "z"]
 var cGuess = alphabet[Math.floor(Math.random() * alphabet.length)]
